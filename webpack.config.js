ec9e2352c027c6803443c61ef6f24d81cc864fa8
Ajan Lal Shrestha <ajan.shrestha@logpoint.com>
2017-06-29 14:44:04 +0545

    Add countdownFrom component and test

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
             applicationStyles: 'app/styles/app.scss',
             Clock: 'app/components/Clock.jsx',
             Countdown: 'app/components/Countdown.jsx',
+            CountdownForm: 'app/components/CountdownForm.jsx',
             Main: 'app/components/Main.jsx',
             Navigation: 'app/components/Navigation.jsx',
             Timer: 'app/components/Timer.jsx'
